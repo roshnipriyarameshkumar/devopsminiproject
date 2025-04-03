039616addce8ceb033c9881d3363d59f618250e7
Roshnipriya <127740197+roshnipriyarameshkumar@users.noreply.github.com>
2025-04-03 22:17:24 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/dev
-    docker push ar8888/dev
+    docker login -u roshnipriya17 -p roshni123
+    docker tag test roshnipriya17/miniproject
+    docker push roshnipriya17/miniproject
     docker-compose up -d
     
